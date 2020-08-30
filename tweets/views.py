5a182a2fc113136047d3738aeb449b865a7f5c5f
Aleksei Grigoruk <ullitap@gmail.com>
2020-08-30 16:12:29 +0300

    48. Two Types of Serializers

--- tweets/views.py
+++ tweets/views.py
@@ -11,7 +11,11 @@ from rest_framework.permissions import IsAuthenticated
 
 from .models import Tweet
 from .forms import TweetForm
-from .serializers import TweetSerializer, TweetActionSerializer
+from .serializers import (
+    TweetSerializer,
+    TweetCreateSerializer, 
+    TweetActionSerializer
+)
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
@@ -24,7 +28,7 @@ def home_view(request, *args, **kwargs):
 #@authentication_classes([SessionAuthentication])
 @permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
-    serializer = TweetSerializer(data = request.POST)
+    serializer = TweetCreateSerializer(data = request.POST)
     if serializer.is_valid(raise_exception=True):
         serializer.save(user=request.user)
         return Response(serializer.data, status=201)
