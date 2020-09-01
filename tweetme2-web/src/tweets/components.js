e12bff2404def29736b6ad7f25c04a2ef3c4ee5d
Aleksei Grigoruk <ullitap@gmail.com>
2020-09-01 14:30:47 +0300

    60. Understanding setState Hook

--- tweetme2-web/src/tweets/components.js
+++ tweetme2-web/src/tweets/components.js
@@ -25,15 +25,25 @@ export function TweetsList(props) {
 
 export function ActionBtn(props) {
     const { tweet, action } = props
+    const [likes, setLikes] = useState(tweet.likes ? tweet.likes : 0)
+    const [userLike, setUserLike] = useState(tweet.userLike === true ? true : false)
     const classname = props.className ? props.className : "btn btn-primary btn-sm"
     const actionDisplay = action.display ? action.display : 'Action'
-    const display = action.type === 'like' ? `${tweet.likes} ${actionDisplay}` : actionDisplay
+
     const handleClick = (event) => {
         event.preventDefault()
         if (action.type === 'like') {
-            console.log(tweet.likes + 1)
+            if (userLike) {
+                setLikes(likes - 1)
+                setUserLike(false)
+            } else {
+                setLikes(likes + 1)
+                setUserLike(true)
+            }
         }
     }
+
+    const display = action.type === 'like' ? `${likes} ${actionDisplay}` : actionDisplay
     return <button className={classname} onClick={handleClick} >{display}</button>
 }
 
