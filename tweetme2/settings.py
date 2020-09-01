38760011170f22f5fdb3cedb986d277f065236a5
Aleksei Grigoruk <ullitap@gmail.com>
2020-09-01 10:10:59 +0300

    55. Handling CORS and Invalid HOST_HEADER

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-(#!^2%*xxlp_3gmk*uhxf+z!@pq!w-=jtjo(7%hyf5#-+--6k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 LOGIN_URL = '/login'
 MAX_TWEET_LENGTH = 240
 TWEET_ACTION_OPTIONS = ['like', 'unlike', 'retweet']
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # third-party
     'rest_framework',
+    'corsheaders',
 
     # internal
     'tweets',
@@ -51,6 +52,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # CorsMiddleware 
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True # any website has access to my api
+CORS_URLS_REGEX = r'^/api/.*$'
 
 DEFAULT_RENDERER_CLASSES = [
     'rest_framework.renderers.JSONRenderer',
