5061c09d04ff7e66a40a98d59cdc534596dc4008
Aleksei Grigoruk <ullitap@gmail.com>
2020-09-01 11:56:27 +0300

    56. Functional Components in React

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -19,6 +19,14 @@ const loadTweets = (callback) => {
   xhr.send()
 }
 
+function Tweet(props) {
+  const { tweet } = props
+  const classname = props.className ? props.className : "col-10 mx-auto col-md-6"
+  return <div className={classname}>
+    <p>{tweet.id} - {tweet.content}</p>
+  </div>
+}
+
 function App() {
   const [tweets, setTweets] = useState([])
 
@@ -42,11 +50,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          {tweets.map((tweet, index) => {
-            return <li>{tweet.content}</li>
+        <div>
+          {tweets.map((item, index) => {
+            return <Tweet tweet={item} key={`${index}-${item.id}`} className="my-5 py-5 border bg-dark text-white" />
           })}
-        </p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
